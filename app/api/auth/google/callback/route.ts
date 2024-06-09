e520afdef68d08fc3b016280402fb901ecb0b057
Mahmudul hasan <157973507+mahmudul-hasan-hridoy@users.noreply.github.com>
2024-06-09 06:09:45 +0000

    Modify

--- app/api/auth/google/callback/route.ts
+++ app/api/auth/google/callback/route.ts
@@ -1,8 +1,10 @@
+// api/auth/gooogle/callback/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { google } from "googleapis";
 import crypto from "crypto";
 import pool from "@/lib/db";
 import sendEmail from "@/lib/sendEmail";
+import jwt from "jsonwebtoken";
 
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
@@ -14,11 +16,11 @@ function generateVerificationToken() {
   return crypto.randomBytes(32).toString("hex");
 }
 
-async function sendVerificationEmail(username, email, verificationToken) {
+async function sendVerificationEmail(name, email, verificationToken) {
   const verificationLink = `${process.env.SITE_URL}/api/auth/verify?token=${verificationToken}`;
   const emailSubject = "Verify your email address";
   const emailHtml = `
-    <p>Dear ${username}</p>
+    <p>Dear ${name}</p>
     <p>Thank you for registering with our service. To complete your registration, please verify your email address by clicking the link below:</p>
     <p><a href="${verificationLink}">Verify Your Email</a></p>
     <p>If you did not create an account, no further action is required.</p>
@@ -36,7 +38,6 @@ async function sendVerificationEmail(username, email, verificationToken) {
 
 export const GET = async (request: NextRequest) => {
   const code = request.nextUrl.searchParams.get("code");
-
   if (!code) {
     return NextResponse.redirect(
       `${process.env.SITE_URL}/auth/register?error=missingCode`,
@@ -46,38 +47,49 @@ export const GET = async (request: NextRequest) => {
   try {
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
-
     const userinfo = await google.oauth2("v2").userinfo.get({
       auth: oauth2Client,
     });
-
     const { email, name, picture } = userinfo.data;
 
     // Check if the user already exists in the database
     const existingUser = await pool.query(
-      "SELECT * FROM google_users WHERE email = $1",
+      "SELECT * FROM users WHERE email = $1",
       [email],
     );
 
     if (existingUser.rowCount > 0) {
       // User already exists, log them in
       console.log("User logged in:", existingUser.rows[0]);
-      // Perform login logic here
+      const token = jwt.sign(
+        { userId: existingUser.rows[0].id },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" },
+      );
+      // Set token as cookie
+      const response = NextResponse.redirect(
+        `${process.env.SITE_URL}/dashboard`,
+      );
+      response.cookies.set("token", token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60,
+      });
+      return response;
     } else {
       // Create a new user
       const verificationToken = generateVerificationToken();
       const userData = {
         email,
-        username: name,
+        name: name,
         avatarUrl: picture,
         verificationToken,
       };
 
       const { rows } = await pool.query(
-        "INSERT INTO google_users (email, username, avatar_url, verification_token) VALUES ($1, $2, $3, $4) RETURNING id",
+        "INSERT INTO users (email, name, avatar_url, verification_token) VALUES ($1, $2, $3, $4) RETURNING id",
         [
           userData.email,
-          userData.username,
+          userData.name,
           userData.avatarUrl,
           userData.verificationToken,
         ],
@@ -86,16 +98,27 @@ export const GET = async (request: NextRequest) => {
       const userId = rows[0].id;
       console.log("User signed up successfully:", userId);
 
+      // Generate JWT token for the new user
+      const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
       // Send verification email
       await sendVerificationEmail(
-        userData.username,
+        userData.name,
         userData.email,
         userData.verificationToken,
       );
 
-      return NextResponse.redirect(
+      // Set token as cookie
+      const response = NextResponse.redirect(
         `${process.env.SITE_URL}/auth/register?success=signedUpWithGoogle`,
       );
+      response.cookies.set("token", token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60,
+      });
+      return response;
     }
   } catch (error) {
     console.error("Error signing up with Google:", error);
@@ -103,6 +126,4 @@ export const GET = async (request: NextRequest) => {
       `${process.env.SITE_URL}/auth/register?error=signUpFailed`,
     );
   }
-
-  return NextResponse.redirect(`${process.env.SITE_URL}/auth/login`);
 };
