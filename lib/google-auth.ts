8c92833d4658a4db1cf97868f8d5c65402bbacf4
Mahmudul hasan <157973507+mahmudul-hasan-hridoy@users.noreply.github.com>
2024-06-10 15:09:35 +0000

    Modify

--- lib/google-auth.ts
+++ lib/google-auth.ts
@@ -5,7 +5,7 @@ import { google } from "googleapis";
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  `${process.env.SITE_URL}/api/auth/google/callback`,
+  `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google/callback`,
 );
 
 export async function getGoogleAuthUrl() {
